41b8df4028bbc06a43e4244661376a75360ba021
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-03-17 15:47:27 -0300

    cambios en dataSource

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -1,22 +1,33 @@
 import { DataSource } from 'typeorm';
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './envs';
+import { DATABASE_URL } from './envs';
 import { User } from '../entities/User';
 import { Credential } from '../entities/Credential';
 import { Order } from '../entities/Order';
 import { Category } from '../entities/Category';
 import { Product } from '../entities/Product';
 
-export const AppDataSource = new DataSource({
-   type: 'postgres',
-   host: DB_HOST,
-   port: DB_PORT,
-   username: DB_USER,
-   password: DB_PASSWORD,
-   database: DB_NAME,
-   synchronize: true,
-   // dropSchema: true,
-   logging: false,
-   entities: [User, Credential, Order, Product, Category],
-   subscribers: [],
-   migrations: [],
-});
+export const AppDataSource = new DataSource(
+   DATABASE_URL
+      ? {
+           type: 'postgres',
+           url: DATABASE_URL, // ✅ Use DATABASE_URL
+           synchronize: true,
+           logging: false,
+           entities: [User, Credential, Order, Product, Category],
+           subscribers: [],
+           migrations: [],
+        }
+      : {
+           type: 'postgres',
+           host: process.env.DB_HOST || 'localhost',
+           port: Number(process.env.DB_PORT) || 5432,
+           username: process.env.DB_USER || 'postgres',
+           password: process.env.DB_PASSWORD || 'admin',
+           database: process.env.DB_NAME || 'proyecto_m4_front',
+           synchronize: true,
+           logging: false,
+           entities: [User, Credential, Order, Product, Category],
+           subscribers: [],
+           migrations: [],
+        }
+);
