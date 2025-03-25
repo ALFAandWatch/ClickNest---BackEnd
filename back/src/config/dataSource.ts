e8975a041b4f868fe8d1551c07eabd0c606d09d2
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-03-25 12:41:08 -0300

    final responsiveness fixes

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -1,33 +1,21 @@
 import { DataSource } from 'typeorm';
-import { DATABASE_URL } from './envs';
+// import { DATABASE_URL } from './envs';
 import { User } from '../entities/User';
 import { Credential } from '../entities/Credential';
 import { Order } from '../entities/Order';
 import { Category } from '../entities/Category';
 import { Product } from '../entities/Product';
 
-export const AppDataSource = new DataSource(
-   DATABASE_URL
-      ? {
-           type: 'postgres',
-           url: DATABASE_URL, // ✅ Use DATABASE_URL
-           synchronize: true,
-           logging: false,
-           entities: [User, Credential, Order, Product, Category],
-           subscribers: [],
-           migrations: [],
-        }
-      : {
-           type: 'postgres',
-           host: process.env.DB_HOST || 'localhost',
-           port: Number(process.env.DB_PORT) || 5432,
-           username: process.env.DB_USER || 'postgres',
-           password: process.env.DB_PASSWORD || 'admin',
-           database: process.env.DB_NAME || 'proyecto_m4_front',
-           synchronize: true,
-           logging: false,
-           entities: [User, Credential, Order, Product, Category],
-           subscribers: [],
-           migrations: [],
-        }
-);
+export const AppDataSource = new DataSource({
+   type: 'postgres',
+   host: 'db.nmmpnkmldxgmuxfwxikw.supabase.co',
+   port: 5432,
+   username: 'postgres',
+   password: 'your_password', // 🔹 Use your real password
+   database: 'postgres',
+   synchronize: true,
+   logging: false,
+   entities: [User, Credential, Order, Product, Category],
+   subscribers: [],
+   migrations: [],
+});
